0a2a7e41be1261583d16c4871ac345af835e3fad
Paulo <paulo_sanches2012@hotmail.com>
2022-04-01 22:23:10 -0300

    created component Main, consuming api and changing image

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { StylesProvider } from '@material-ui/styles';
 import { CssBaseline } from '@material-ui/core';
 import GlobalStyles from './commons/styles/global-styles';
+import Main from './pages/Main';
 
 function App() {
   return (
     <StylesProvider injectFirst>
       <CssBaseline />
       <GlobalStyles />
-      <div>Teste</div>
+      <Main />
     </StylesProvider>
   );
 }
---
+++ src/pages/Main/index.jsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+
+function Main() {
+  return (
+    <>
+      <h1>Main</h1>
+    </>
+  );
+}
+
+export default memo(Main);
