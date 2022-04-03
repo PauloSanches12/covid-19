8003380d02cfbcbe6806f524f61e8eab5bfc5a9c
Paulo <paulo_sanches2012@hotmail.com>
2022-04-03 20:56:57 -0300

    showing information from other countries

---
+++ src/pages/Main/components/Panel/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { CardContent } from '../../../../components/';
+
+export const CardPanelContent = styled(CardContent)`
+    display: flex;
+    justify-content: space-between;
+    padding: 25px;
+`
+
+export const Item = styled.div`
+    display: flex;
+    justify-content: space-between;
+    min-width: 150px;
+`
